7cfbb7fced1d905a440522fa954757c49df80652
mjcampagna <mjcampagna@gmail.com>
2018-05-20 15:48:16 -0700

    Repo & project reset, then setup

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  watch: false,
+  
+  module: {
+    rules: [
+
+      { 
+        test: /\.jsx?$/, 
+        loader: 'babel-loader',
+        include: /src/,
+        options: {
+          presets: ['env', 'react']
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+
+    ]//rules
+  },//module
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      template: 'src/index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+
+
+  ]//plugins
+
+};
